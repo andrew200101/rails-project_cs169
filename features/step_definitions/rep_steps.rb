f2eb8c0964a66104dfeef738e8ac6067c6335de0
Pranathi Peri <pranathiperi@berkeley.edu>
2021-11-30 01:43:39 +0000

    fixed linter to make build pass

--- features/step_definitions/rep_steps.rb
+++ features/step_definitions/rep_steps.rb
@@ -1,13 +1,9 @@
-Then /(.*) representatives should exist in the db/ do | n_seeds |
-  expect(Representative.count).to eq n_seeds.to_i
+# frozen_string_literal: true
+
+Then(/(.*) representatives should exist in the db/) do |n_seeds|
+    expect(Representative.count).to eq n_seeds.to_i
 end
 
-Then /^debug$/ do
-  # Use this to write "Then debug" in your scenario to open a console.
-   require "byebug"; byebug
-  1 # intentionally force debugger context in this method
-end
-
-Then /the db should not contain dupes/ do
-  expect(Representative.where("name == 'Kamala D. Harris'").count).to eq 1
+Then(/the db should not contain dupes/) do
+    expect(Representative.where("name == 'Kamala D. Harris'").count).to eq 1
 end
