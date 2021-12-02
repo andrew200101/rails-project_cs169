376dc77cd8a62f18e386a17c42af68e4449875be
Rithvik Sunku <rithviksunku@berkeley.edu>
2021-12-02 00:30:12 +0000

    task 2 modifications untested

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -9,16 +9,31 @@ class Representative < ApplicationRecord
         rep_info.officials.each_with_index do |official, index|
             ocdid_temp = ''
             title_temp = ''
+            photoUrl_temp = ''
+            line1_temp = ''
+            city_temp = ''
+            state_temp = ''
+            zip_temp = ''
+            party_temp = ''
+            
 
             rep_info.offices.each do |office|
                 if office.official_indices.include? index
                     title_temp = office.name
                     ocdid_temp = office.division_id
+                    photoUrl_temp_temp = office.photoUrl
+                    line1_temp = office.line1
+                    city_temp = office.city 
+                    state_temp = office.state
+                    zip_temp = office.zip 
+                    party_temp = office.party
+                    
                 end
             end
 
             rep = Representative.find_or_create_by!({ name: official.name, ocdid: ocdid_temp,
-                title: title_temp })
+                title: title_temp, photoUrl: photoUrl_temp, line1: line1_temp,  city: city_temp, 
+                state: state_temp, zip: zip_temp, party: party_temp})
             reps.push(rep)
         end
 
